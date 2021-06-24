bcee21be942c5338859ab5da840c3a87eda46376
rcurinz <53925652+rcurinz@users.noreply.github.com>
2021-06-24 17:24:38 -0400

    deteccion facial

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-from cv2 import *
-
-print ("hola mundo")
-print ("hola k ase")
+# import the opencv library
+import cv2
 
 
-namedWindow("webcam")
-vc = VideoCapture(0);
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+# define a video capture object
+vid = cv2.VideoCapture(0)
 
-while True:
-    next, frame = vc.read()
-    imshow("webcam", frame)
-    if waitKey(50) >= 0:
+while (True):
+    ret, frame = vid.read()
+    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(frame, 1.1, 4)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+vid.release()
+cv2.destroyAllWindows()
