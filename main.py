a3be1bf8d93fcb796f97993ff80fc3f0e58418db
Rene <rcurin2013@alu.uct.cl>
2021-06-24 04:07:47 -0400

    add

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+from cv2 import *
+
 print ("hola mundo")
 print ("hola k ase")
+
+
+namedWindow("webcam")
+vc = VideoCapture(0);
+
+while True:
+    next, frame = vc.read()
+    imshow("webcam", frame)
+    if waitKey(50) >= 0:
+        break
